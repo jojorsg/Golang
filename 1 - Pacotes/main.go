4aa6d79254f66bf5d9165b01819867901e22e0e2
jojorsg <josueroberto099@gmail.com>
2024-02-06 14:09:17 -0300

    Add módulos

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("josueroberto099@gmail.com")
+	fmt.Println(erro)
+
+	erro2 := checkmail.ValidateFormat("741")
+	fmt.Println(erro2)
 }
